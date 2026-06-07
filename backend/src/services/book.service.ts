32c272a259e78eb147d298c480b50d760951ae65
Yuval Topaz <yuvaltopaz87@gmail.com>
2026-06-07 14:32:19 +0300

    first stage

--- backend/src/services/book.service.ts
+++ backend/src/services/book.service.ts
@@ -6,12 +6,4 @@ export class BookService {
   getAllBooks() {
     return bookRepository.findAll();
   }
-
-  getBookById(id: number) {
-    return bookRepository.findById(id);
-  }
-
-  createBook(data: { title: string; author_id: number; release_date: Date }) {
-    return bookRepository.create(data);
-  }
 }
