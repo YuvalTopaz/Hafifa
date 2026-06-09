846a3fe194ff1bc9d4e5882a6b8bd91862da3684
Yuval Topaz <yuvaltopaz87@gmail.com>
2026-06-09 14:39:59 +0300

    add env

--- frontend/src/Pages/HomePage/HomePage.tsx
+++ frontend/src/Pages/HomePage/HomePage.tsx
@@ -6,20 +6,13 @@ import type { Book } from "../../Types";
 export default function HomePage() {
   const { books, isLoading, error } = useBooks();
 
-  if (isLoading) {
-    return <div>Loading...</div>;
-  }
-
-  if (error) {
-    return <div>{error}</div>;
-  }
-
   return (
     <>
       <div className="container mt-4">
         <CatalogHeader />
       </div>
       <div className="container">
+        {isLoading ? "Loading..." : error ? <div>{error}</div> : null}
         <div className="row justify-content-start g-3">
           {books.map((book: Book) => (
             <div key={book.id} className="col-12 col-md-6 col-lg-4">
